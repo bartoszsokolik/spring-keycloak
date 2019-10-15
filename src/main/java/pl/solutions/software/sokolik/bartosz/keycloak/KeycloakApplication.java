305822623ad16015304505931d7261d55c3eaa8c
Bartosz Sokolik <bartosz.sokolik@gmail.com>
2019-10-15 21:27:57 +0200

    Add basic keycloak configuration

---
+++ src/main/java/pl/solutions/software/sokolik/bartosz/keycloak/KeycloakApplication.java
@@ -0,0 +1,13 @@
+package pl.solutions.software.sokolik.bartosz.keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakApplication.class, args);
+	}
+
+}
